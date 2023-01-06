a8401141c2c17923913f6314291a4060a74461a2
Edgar Tapia <55415136+edtap14@users.noreply.github.com>
2023-01-05 23:37:55 -0600

    se configuro el servicio para subir imagen a firebase

---
+++ src/api/storage.js
@@ -0,0 +1,14 @@
+import { getStorage, ref, uploadBytes } from 'firebase/storage'
+
+export class Storage {
+  async uploadFile (file, folder, nameFile) {
+    try {
+      const storage = getStorage()
+      console.log(storage)
+      const fileRef = ref(storage, `${folder}/${nameFile}`)
+      return await uploadBytes(fileRef, file)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
--- src/components/Profile/AvatarUpdate/AvatarUpdate.js
+++ src/components/Profile/AvatarUpdate/AvatarUpdate.js
@@ -1,12 +1,28 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { Image } from 'semantic-ui-react'
+import { User, Storage } from '../../../api'
 import { useDropzone } from 'react-dropzone'
 import { defaultUser } from '../../../assets'
 import './AvatarUpdate.scss'
 
+const userController = new User()
+const storageController = new Storage()
+
 export function AvatarUpdate () {
+  const { photoURL, uid } = userController.getMe()
+
+  const [avatarUrl, setAvatarUrl] = useState(photoURL || defaultUser)
+
   const onDrop = useCallback(async acceptedFile => {
-    console.log(acceptedFile)
+    const file = acceptedFile[0]
+    console.log({ file })
+    setAvatarUrl(URL.createObjectURL(file))
+    const response = await storageController.uploadFile(
+			file,
+			'avatar',
+			uid
+		)
+    console.log(response)
   })
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop })
@@ -14,7 +30,7 @@ export function AvatarUpdate () {
   return (
     <div className='avatar-update' {...getRootProps()}>
       <input {...getInputProps()} />
-      <Image src={defaultUser} />
+      <Image src={avatarUrl} />
     </div>
   )
 }
